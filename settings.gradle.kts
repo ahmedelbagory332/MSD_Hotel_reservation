cdef5d9cb0f4d1174a111bdd759ce7c9859c0153
ahmedelbagory332 <ahmedelbagory63@gmail.com>
2026-04-15 18:22:18 +0200

    Initial project

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MSD - Hotel reservation"
+include(":app")
+include(":search_auto_complete_feature")
